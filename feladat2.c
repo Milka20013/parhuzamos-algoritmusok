e7360148ff3535a01c10414725108c3921f3a8b4
Bordás Milán <63600211+Milka20013@users.noreply.github.com>
2023-03-06 13:30:17 +0100

    Add files via upload

--- feladat2.c
+++ feladat2.c
@@ -2,18 +2,45 @@
 #include<math.h>
 #include<stdlib.h>
 #include<time.h>
+#include<windows.h>
 
 int generateRandomInInterval(int a, int b);
 
 int main()
 {
-    int n, a, b;
+    int n,m, a, b, number;
+    printf("Elemszam :");
+    scanf("%d",&n);
 
-    scanf("%d %d %d",n,a,b);
-    for (int i = 0; i < n; i++)
+    printf("Elemszam szorzo :");
+    scanf("%d",&m);
+    short numbers[n];
+    clock_t begin = clock();
+    srand(time(NULL));
+    for (int i = 0; i < m; i++)
     {
-        printf("%d",generateRandomInInterval(a,b));
+        for (int j = 0; j < n; j++)
+        {
+            numbers[j] = generateRandomInInterval(1,100);
+        }
     }
+    
+    FILE *fp;
+    fp = fopen("asd.txt","w");
+    clock_t middle = clock();
+    for (int j = 0; j < m;j++)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            fprintf(fp,"%d \n",numbers[i]);
+        }
+    }
+    
+   
+    fclose(fp);
+    clock_t end = clock();
+    printf("Time to generate numbers: %lf \n",(double)(middle-begin)/CLOCKS_PER_SEC);
+    printf("Time to write numbers: %lf \n",(double)(end-middle)/CLOCKS_PER_SEC);
     
     return 0;
 }
@@ -24,7 +51,7 @@ int generateRandomInInterval(int a, int b)
         return -1;
     }
 
-    srand(time);
-    int num = rand();
+    
+    int num = rand() % (b-a + 1) + a;
     return num;
 }
