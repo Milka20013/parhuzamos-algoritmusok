9cc9948491121ba292f092bced84119806541611
Bordás Milán <63600211+Milka20013@users.noreply.github.com>
2023-04-24 13:22:20 +0200

    Add files via upload

--- posixBead.c
+++ posixBead.c
@@ -5,159 +5,75 @@
 #include <time.h>
 #include <unistd.h>
 
-const int N_THREADS = 2;
-
 int GenerateRandomBetween(int a, int b);
 double GenerateRandomDBetween(double a, double b);
 
-void *RandomSort(void *param);
-void RandomSortS(int *array, int length);
-int Sorted(int *array, int length);
-void RandomIndexArray(int *array, int length);
-int Unique(int *array, int length);
-
 typedef struct
 {
+    char* content;
     int length;
-    int array[];
-} ThreadParam;
+}String;
+
+void SearchString(String text, String str, int *indices);
 
 int main()
 {
     srand(time(NULL));
-    /*pthread_t threads[N_THREADS];
-    int length = 10;
-    ThreadParam *threadParamPtr = malloc(sizeof(ThreadParam) + length * sizeof(int));
-    for (int i = 0; i < length; i++)
+
+    int numberOfLetters = 50000;
+    String* textPtr = malloc(sizeof(String));
+    textPtr->content = malloc(sizeof(char)*numberOfLetters);
+    textPtr->length = numberOfLetters;
+
+    FILE *fp = fopen("lorem.txt","r");
+    if(fp == 0)
     {
-       threadParamPtr->array[i] = GenerateRandomBetween(0,length);
+        printf("Couldn't open the file");
+        return 0;
     }
-    threadParamPtr->length = length;
-    for (int i = 0; i < N_THREADS; ++i)
+    char tmp;
+    for (int i = 0; i < numberOfLetters; i++)
     {
-        pthread_create(&threads[i], NULL, RandomSort, threadParamPtr);
+        tmp = fgetc(fp);
+        textPtr->content[i] = tmp;
+        printf("%c",textPtr->content[i]);
+        
     }
-    while (!Sorted(threadParamPtr->array, length))
-    {
-        for (int i = 0; i < length; i++)
-        {
-            printf(" %d ",threadParamPtr->array[i]);
-        }
-    }*/
-    int length = 5;
-    int arr[length];
-    for (int i = 0; i < length; i++)
-    {
-        arr[i] = GenerateRandomBetween(0,100);
-    }
+    fclose(fp);
+    free(textPtr);
+    char strToFind[] = "Lorem";
     
-    RandomSortS(arr, length);
-    for (int i = 0; i < length; i++)
-    {
-        printf(" %d ", arr[i]);
-    }
+    
+    
+
 
     return 0;
 }
 
-int GenerateRandomBetween(int min, int max)
+void SearchString(String text, String str, int *indices)
 {
-    if (min >= max)
+    int tester = 0;
+    int indexOfIndex = 0;
+    for (int i = 0; i < text.length - str.length; i++)
     {
-        return min - 1;
-    }
-    return rand() % (max - min + 1) + min;
-}
-
-double GenerateRandomDBetween(double min, double max)
-{
-    if (min >= max)
-    {
-        return min - 1;
-    }
-    double range = RAND_MAX / (max - min);
-    return rand() / range + min;
-}
-
-void RandomSortS(int *array, int length)
-{
-    int arr[length];
-    int indexArr[length];
-    for (int i = 0; i < length; i++)
-    {
-        arr[i] = array[i];
-        indexArr[i] = i;
+        for (int j = 0; j < str.length; j++)
+        {
+            if (str.content[j] != text.content[i + j])
+            {
+                break;
+            }
+            else
+            {
+                tester ++;
+            }
+            
+        }
+        if (tester == str.length)
+        {
+            indices[indexOfIndex] = i;
+            indexOfIndex ++;
+        }
     }
     
-    while (Sorted(arr, length) == 0)
-    {
-        RandomIndexArray(indexArr, length);
-        for (int i = 0; i < length; i++)
-        {
-            arr[indexArr[i]] = array[indexArr[i]];
-        }
-    }
-    for (int i = 0; i < length; i++)
-    {
-        array[i] = arr[i];
-    }
-}
 
-void *RandomSort(void *param)
-{
-    ThreadParam *threadParam = (ThreadParam *)param;
-    int length = threadParam->length;
-    int arr[length];
-    int indexArr[length];
-    while (Sorted(arr, length) == 0)
-    {
-        RandomIndexArray(indexArr, length);
-        for (int i = 0; i < length; i++)
-        {
-            arr[indexArr[i]] = threadParam->array[indexArr[i]];
-        }
-    }
-    for (int i = 0; i < length; i++)
-    {
-        threadParam->array[i] = arr[i];
-    }
-}
-
-int Sorted(int *array, int length)
-{
-    for (int i = 0; i < length - 1; i++)
-    {
-        if (array[i] > array[i + 1])
-        {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void RandomIndexArray(int *array, int length)
-{
-    while (Unique(array, length) == 0)
-    {
-        for (int i = 0; i < length; i++)
-        {
-            array[i] = GenerateRandomBetween(0, length - 1);
-        }
-    }
-}
-
-int Unique(int *array, int length)
-{
-    for (int i = 0; i < length - 1; i++)
-    {
-        for (int j = i + 1; j < length; j++)
-        {
-            if (array[i] == array[j])
-            {
-                return 0;
-            }
-        }
-    }
-
-    return 1;
 }
